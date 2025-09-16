8105e64effb5d5fe485444f51039b3056e0f6bc8
erab2r <shkera213@gmail.com>
2025-09-16 22:23:49 +0600

    first commit

---
+++ spread.js
@@ -0,0 +1,8 @@
+const friends = ['Alice', 'Bob', 'Charlie'];
+console.log(friends);
+console.log(...friends);
+const newFriends = ['David', 'Eve', ...friends];
+console.log(newFriends);
+const numbers =[34,37,38,456,567,999];
+console.log(Math.max(numbers));
+console.log(Math.max(...numbers));
