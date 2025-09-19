078aeece2c648fa16753a93ea576b4d499d74e7a
erab2r <shkera213@gmail.com>
2025-09-19 18:10:32 +0600

    1

--- practice/post.js
+++ practice/post.js
@@ -1,7 +1,22 @@
+// post.js
 const handlePost = () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data =>{
-        console.log(data);
-    })
-}
+        .then(res => res.json())
+        .then(data => {
+            displayPost(data);
+        });
+};
+
+const displayPost = (posts) => {
+    const postContainer = document.getElementById('postContainer');
+    for (const post of posts) {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+            <h1>${post.title}</h1>
+            <p>${post.body}</p>
+            <button>Details</button>
+        `;
+        postContainer.appendChild(div);
+    }
+};
