fe2eb07ed50f15a32b3168f0518d9d938e2b8d97
erab2r <shkera213@gmail.com>
2025-09-23 03:22:49 +0600

    1

--- practice/async.js
+++ practice/async.js
@@ -1,7 +1,21 @@
-const handleUser = () =>{
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(data =>console.log(data));
+// const handleUser = () =>{
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(res => res.jon())
+//     .then(data =>console.log(data))
+//     .catch(error =>console.log("error dhorci",error));
+    
+// }
+
+const handleUser = async () =>{
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json;
+        console.log(data);
+    }
+    catch{
+        console.log("helo");
+    }
 }
+
 
 handleUser()
